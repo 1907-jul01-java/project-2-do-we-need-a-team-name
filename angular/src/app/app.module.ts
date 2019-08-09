3675ba19bf5f99b38676f2c74c23d7aaa75ed224
Sean McClanahan <s.mcclanahan96@gmail.com>
2019-08-08 19:18:34 -0500

    test

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormBuilder } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormBuilder
   ],
   providers: [],
   bootstrap: [AppComponent]
