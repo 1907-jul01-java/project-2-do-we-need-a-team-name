09b25fa651ec6a9d6fab7796ef2a42b0cb8c73e3
Sean McClanahan <s.mcclanahan96@gmail.com>
2019-08-14 15:27:18 -0500

    fixed data binding

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HomeServiceService } from '../home-service.service';
 import { ConfigService } from '../config.service';
 import { map } from 'rxjs/operators';
-
+import { Observable } from 'rxjs';
+import { MovieInfo } from './MovieInfo';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,7 +13,8 @@ import { map } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
 
   constructor(private HomeService: HomeServiceService, private ConfigService: ConfigService) { }
-  nowPlaying;
+  @Input() nowPlaying;
+  nowPlayingDisplay;
   topRated;
   Popular;
 
@@ -20,13 +22,18 @@ export class HomeComponent implements OnInit {
     // this.nowPlaying = this.HomeService.getNowPlaying();
     // console.log(this.nowPlaying);
     // this.nowPlaying.imageURL = (this.ConfigService.getListImage() + this.nowPlaying.poster_path);
-    this.HomeService.getNowPlaying().pipe(map( response => {
-      this.nowPlaying = response;
-      this.nowPlaying.imageURL = (this.ConfigService.getListImage() + this.nowPlaying.results.poster_path);
-      console.log(this.nowPlaying.results);
-    })).subscribe();
+    // this.HomeService.getNowPlaying().pipe(map( response => {
+    //   this.nowPlaying = response;
+    //   this.nowPlaying.imageURL = (this.ConfigService.getListImage() + this.nowPlaying.results.poster_path);
+    //   console.log(this.nowPlaying.results);
+    // })).subscribe();
+    this.HomeService.getNowPlaying().subscribe(response => {
+      this.nowPlaying = response['results'];
+      
+      });
+      console.log(this.nowPlaying);
     this.topRated = this.HomeService.getTopRated();
     this.Popular = this.HomeService.getPopular();
-   }
+  }
 
 }
