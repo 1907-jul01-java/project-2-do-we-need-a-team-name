8fef4392eef0c11cfb7f8da3126f63aa3a1898c9
superuser840 <Chodgson888@gmail.com>
2019-08-15 04:03:20 -0500

    sleep

--- angular/src/app/tracker/tracker.component.ts
+++ angular/src/app/tracker/tracker.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TrackerService } from '../tracker.service';
+import { FormGroup } from '@angular/forms';
+import { MovieTracker } from './MovieTracker';
 
 @Component({
   selector: 'app-tracker',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackerComponent implements OnInit {
 
-  constructor() { }
+  MovieForm: FormGroup;
+  userInfo;
+  movieTracker: MovieTracker;
+
+  constructor(private tracker: TrackerService) { }
+  @Input() Tracked;
+  Watched;
+  // topRated;
+  // Popular;
 
   ngOnInit() {
+    this.tracker.getTracked(this.movieTracker.username).subscribe(response => {
+      this.Tracked = response['results'];
+      console.log(this.Tracked)
+      });
+    this.tracker.getWatched(this.movieTracker.username).subscribe(response => {
+      this.Watched = response['results'];
+      console.log(this.Watched);
+    });
+    // this.HomeService.getTopRated().subscribe(response => {
+    //   this.topRated = response['results'];
+    //   console.log(this.topRated);
+    // });
   }
 
 }
